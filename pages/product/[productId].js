f0256b2268ab95a9705d8ba9197ce600c44882ef
Dharmjeet Vaishnav <dharmjeetvaishnav@gmail.com>
2021-07-27 21:59:04 +0530

    Dynamic route Done

---
+++ pages/product/[productId].js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export default function ProductDetail() {
+  const router = useRouter();
+  const productId = router.query.productId;
+
+  return <h1>Details about product {productId}</h1>;
+}
---
+++ pages/product/index.js
@@ -0,0 +1,9 @@
+export default function index() {
+  return (
+    <>
+      <h2>Product 1</h2>
+      <h2>Product 2</h2>
+      <h2>Product 3</h2>
+    </>
+  );
+}
